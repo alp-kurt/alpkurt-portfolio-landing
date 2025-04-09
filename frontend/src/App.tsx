ccb4e15d41f17961a14864d513f61e772e5a1e59
Alp Kurt <krtalp@gmail.com>
2025-04-09 02:45:21 +0200

    feat: integrate custom firebase events

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,81 @@
 import pacmanLogo from "./assets/pacman.png";
 import webLogo from "./assets/web.png";
-import "./App.css";
-import Ghost from './components/Ghost';
+import './App.css';
+import Ghost from "./components/Ghost";
+import { logCustomEvent } from "./analytics";
 
 function App() {
+  const handleClick = (eventName: string, url: string) => {
+    logCustomEvent(eventName);
+    window.open(url, "_blank");
+  };
+
   return (
     <>
-    <Ghost /> <Ghost /> <Ghost />
+      <Ghost /> <Ghost /> <Ghost />
       <div>
         <h1>Alp Kurt's Personal Website</h1>
         <p>-- currently rebuilding the app --</p>
-        <a target="_blank" href="https://webdeveloper.alpkurt.com">
-          <img src={webLogo} className="logo" alt="Web App logo" />
-        </a>
-        <a target="_blank" href="https://gamedeveloper.alpkurt.com">
-          <img src={pacmanLogo} className="logo react" alt="Pacman logo" />
-        </a>
-      </div>
+        <img
+          src={webLogo}
+          className="logo"
+          alt="Web App logo"
+          onClick={() =>
+            handleClick(
+              "clicked_web_portfolio",
+              "https://webdeveloper.alpkurt.com"
+            )
+          }
+          style={{ cursor: "pointer" }}
+        />
 
+        <img
+          src={pacmanLogo}
+          className="logo react"
+          alt="Pacman logo"
+          onClick={() =>
+            handleClick(
+              "clicked_game_portfolio",
+              "https://gamedeveloper.alpkurt.com"
+            )
+          }
+          style={{ cursor: "pointer" }}
+        />
+      </div>
       <div className="card">
         <p>portfolio links 👆</p>
         <p>professional links👇</p>
       </div>
       <div className="links-group">
         <p>
-          •
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alp-kurt/">LinkedIn</a> •{" "}
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/pabron7">GitHub</a> •{" "}
-          <a target="_blank" rel="noopener noreferrer" href="https://pabron.itch.io/">Itch</a> 
-          •
+          •{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/alp-kurt/"
+            onClick={() => logCustomEvent("LinkedIn")}
+          >
+            LinkedIn
+          </a>{" "}
+          •{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/pabron7"
+            onClick={() => logCustomEvent("clicked_github")}
+          >
+            GitHub
+          </a>{" "}
+          •{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://pabron.itch.io/"
+            onClick={() => logCustomEvent("clicked_itch")}
+          >
+            Itch
+          </a>
+          {" "}•
         </p>
       </div>
     </>
